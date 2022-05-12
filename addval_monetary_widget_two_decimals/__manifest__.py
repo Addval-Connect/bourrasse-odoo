745d0c3b1e54ce082c3f7a51974a1e3206947ab0
DHerrmannP <dherrmann@addval.cl>
2022-05-12 17:03:54 -0400

    added custom monetary widget

---
+++ addval_monetary_widget_two_decimals/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Monetary Widget with fixed two digits (Addval)",
+    "summary": "Monetary Widget with fixed two digits (Addval)",
+    "category": "Customization",
+    "description": "This module will make available a monetary widget wich always shows thwo decimal places.",
+    "depends": ["base"],
+    "version": "1.0",
+    "auto_install": False,
+    "license": "LGPL-3",
+    "author": "Addval Connect",
+    "website": "http://www.addval.cl",
+    "data": [],
+    "installable": True,
+    "application": True,
+}
