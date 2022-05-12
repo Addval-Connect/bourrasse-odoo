510c93ef51c790bec929c4779bae392225995a06
JT Guzman <jtguzman@addval.cl>
2022-05-11 21:04:55 -0400

    Update model structure

--- addval_update_invoice_references/__manifest__.py
+++ addval_update_invoice_references/__manifest__.py
@@ -2,18 +2,18 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Custom Vendor bills report (Addval)',
-    'summary': 'Custom Vendor bills report (Addval)',
-    'author': 'Odoo PS',
+    'name': 'Addval Update Invoice References',
+    'summary': 'Updates references for invoices',
+    'author': 'Addval Connect',
     'category': 'Customization',
-    'description': "This module will change the vendor bill report layout.",
+    'description': "This module will update the invoice references.",
     'depends': ['account', 'l10n_cl', 'l10n_cl_edi'],
     'version': '1.0',
     'auto_install': False,
     'license': 'LGPL-3',
     'website': "http://www.odoo.com",
     'data': [
-        'views/report_invoice.xml',
+        'data/l10n_latam.document.type.csv',
     ],
     'installable': True,
     'application': True,
