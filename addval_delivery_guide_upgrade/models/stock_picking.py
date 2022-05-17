89105fd09b7af985d41df7cb0139bd6fc1025a7d
DHerrmannP <dherrmann@addval.cl>
2022-05-17 18:05:25 -0400

    test change to check for parent_id in invoice

--- addval_delivery_guide_upgrade/models/stock_picking.py
+++ addval_delivery_guide_upgrade/models/stock_picking.py
@@ -4,8 +4,3 @@ from odoo import models
 class Picking(models.Model):
     _name = "stock.picking"
     _inherit = ["stock.picking"]
-
-    # def print_delivery_guide_pdf(self):
-    #     return self.env.ref(
-    #         "addval_delivery_guide_upgrade.action_delivery_guide_report_pdf"
-    #     ).report_action(self)
