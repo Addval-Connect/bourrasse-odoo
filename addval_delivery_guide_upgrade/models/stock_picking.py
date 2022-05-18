c6937e639cefa461b57c6e35b7e5dada1c53cecd
DHerrmannP <dherrmann@addval.cl>
2022-05-18 11:11:42 -0400

    maybe not needed

--- addval_delivery_guide_upgrade/models/stock_picking.py
+++ addval_delivery_guide_upgrade/models/stock_picking.py
@@ -5,5 +5,5 @@ class Picking(models.Model):
     _name = "stock.picking"
     _inherit = ["stock.picking"]
 
-    def _get_dte_template(self):
-        return self.env.ref("addval_delivery_guide_upgrade.dte_template")
+    # def _get_dte_template(self):
+    #     return self.env.ref("addval_delivery_guide_upgrade.dte_template")
