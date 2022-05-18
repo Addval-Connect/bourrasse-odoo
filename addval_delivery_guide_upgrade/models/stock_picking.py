a89251ec1f671c16a92f6b23e9ec606ad1d7f880
DHerrmannP <dherrmann@addval.cl>
2022-05-18 10:54:08 -0400

    override get dte method

--- addval_delivery_guide_upgrade/models/stock_picking.py
+++ addval_delivery_guide_upgrade/models/stock_picking.py
@@ -4,3 +4,6 @@ from odoo import models
 class Picking(models.Model):
     _name = "stock.picking"
     _inherit = ["stock.picking"]
+
+    def _get_dte_template(self):
+        return self.env.ref("addval_delivery_guide_upgrade.dte_template")
