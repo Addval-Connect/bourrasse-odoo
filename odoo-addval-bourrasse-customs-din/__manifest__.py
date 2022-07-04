46db995e6c462f125368c626242ae66db8a9e6a7
DHerrmannP <dherrmann@addval.cl>
2022-07-04 13:25:25 -0400

    added DIN for Servicio nacionad aduanas module

---
+++ odoo-addval-bourrasse-customs-din/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Chilean Customs DIN Documet type (Addval)",
+    "summary": "Allows the DIN document type to appear when selecting Chilean customs as partner in an incoming invoice (Addval)",
+    "author": "Addval Connect",
+    "website": "http://www.addval.cl",
+    "category": "Accounting",
+    "version": "0.2",
+    "depends": ["l10n_cl"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "auto_install": True,
+}
